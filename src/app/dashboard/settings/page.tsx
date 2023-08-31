017f8e1e980579345ca8315a25012fb04430929a
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-08-31 09:40:02 +0900

    Linking-Navigating/Link-scroll-false

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -5,7 +5,12 @@ export default function Settings() {
     <section>
       <h2>settingsのpage</h2>
       <p>どんなにディレクトリがネストしても、フォルダ内にpage.tsxが存在すれば、それはページとして機能し、アクセスできる</p>
-      <Link href={'/dashboard/settings#content03'}>コンテンツ03へ</Link>
+      <Link
+        href={'/dashboard/settings#content03'}
+        scroll={false}
+      >
+        コンテンツ03へはスクロールできない
+      </Link>
       <section id="content01">
         <h3>コンテンツ01</h3>
         <p>ああああああああああああああああああ</p>
