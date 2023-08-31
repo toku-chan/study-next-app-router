ff1c9829357583f5025dc42cbe2965862690d489
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-08-31 09:39:13 +0900

    Linking-Navigating/Link-scroll-id

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,8 +1,32 @@
+import Link from "next/link"
+
 export default function Settings() {
   return(
     <section>
       <h2>settingsのpage</h2>
       <p>どんなにディレクトリがネストしても、フォルダ内にpage.tsxが存在すれば、それはページとして機能し、アクセスできる</p>
+      <Link href={'/dashboard/settings#content03'}>コンテンツ03へ</Link>
+      <section id="content01">
+        <h3>コンテンツ01</h3>
+        <p>ああああああああああああああああああ</p>
+        <p>ああああああああああああああああああ</p>
+        <p>ああああああああああああああああああ</p>
+        <p>ああああああああああああああああああ</p>
+      </section>
+      <section id="content02">
+        <h3>コンテンツ02</h3>
+        <p>ああああああああああああああああああ</p>
+        <p>ああああああああああああああああああ</p>
+        <p>ああああああああああああああああああ</p>
+        <p>ああああああああああああああああああ</p>
+      </section>
+      <section id="content03">
+        <h3>コンテンツ03</h3>
+        <p>ああああああああああああああああああ</p>
+        <p>ああああああああああああああああああ</p>
+        <p>ああああああああああああああああああ</p>
+        <p>ああああああああああああああああああ</p>
+      </section>
     </section>
   )
 }
